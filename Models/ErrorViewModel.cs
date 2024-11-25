6b08961b346dfd949e75e75a2fccd519fa974261
DivyakolaSriSai <srisaidivyakola@gmail.com>
2024-11-25 14:16:10 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMG_db.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
